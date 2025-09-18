a2e01347bef0065317b9d3d0e2eabd3786f0ac5c
pdefusco <pauldefusco@gmail.com>
2025-09-18 14:45:01 -0700

    added import os

--- model_evaluation/xgboost_model_eval.py
+++ model_evaluation/xgboost_model_eval.py
@@ -8,6 +8,7 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
+import os
 
 # ----------------------------
 # 1. Create synthetic data
