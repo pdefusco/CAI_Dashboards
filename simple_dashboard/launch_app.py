d75c76f4b4df9462ccb13a5e048cc3dec70bc763
pdefusco <pauldefusco@gmail.com>
2026-02-24 15:54:52 -0800

    updated port

--- simple_dashboard/launch_app.py
+++ simple_dashboard/launch_app.py
@@ -4,6 +4,6 @@ import os
 if __name__ == "__main__":
     app.run_server(
         host="127.0.0.1",
-        port=int(os.environ.get("CDSW_APP_PORT")),
+        port=8080,
         debug=True
     )
